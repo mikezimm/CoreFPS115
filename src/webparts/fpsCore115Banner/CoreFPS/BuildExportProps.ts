44de86256939b44565a89e51ba6a17874659589b
mikezimm <49648086+mikezimm@users.noreply.github.com>
2022-08-08 01:29:32 -0400

    Update BuildExportProps.ts

--- src/webparts/fpsCore115Banner/CoreFPS/BuildExportProps.ts
+++ src/webparts/fpsCore115Banner/CoreFPS/BuildExportProps.ts
@@ -53,26 +53,6 @@ import { changePinMe,  } from '../fpsReferences';
 import { importBlockProps } from '../IFpsCore115BannerWebPartProps'
 
 /***
- *    .d8888.  .d8b.  .88b  d88. d8888b. db      d88888b       .d88b.  d8b   db db      db    db                                                                            
- *    88'  YP d8' `8b 88'YbdP`88 88  `8D 88      88'          .8P  Y8. 888o  88 88      `8b  d8'                                                                            
- *    `8bo.   88ooo88 88  88  88 88oodD' 88      88ooooo      88    88 88V8o 88 88       `8bd8'                                                                             
- *      `Y8b. 88~~~88 88  88  88 88~~~   88      88~~~~~      88    88 88 V8o88 88         88                                                                               
- *    db   8D 88   88 88  88  88 88      88booo. 88.          `8b  d8' 88  V888 88booo.    88                                                                               
- *    `8888Y' YP   YP YP  YP  YP 88      Y88888P Y88888P       `Y88P'  VP   V8P Y88888P    YP                                                                               
- *                                                                                                                                                                          
- *                                                                                                                                                                          
- *     .o88b.  .d88b.  d8888b. db    db      d888888b  .d88b.       db       .d88b.   .o88b.  .d8b.  db           d8888b. d8888b.  .d88b.     d88b d88888b  .o88b. d888888b 
- *    d8P  Y8 .8P  Y8. 88  `8D `8b  d8'      `~~88~~' .8P  Y8.      88      .8P  Y8. d8P  Y8 d8' `8b 88           88  `8D 88  `8D .8P  Y8.    `8P' 88'     d8P  Y8 `~~88~~' 
- *    8P      88    88 88oodD'  `8bd8'          88    88    88      88      88    88 8P      88ooo88 88           88oodD' 88oobY' 88    88     88  88ooooo 8P         88    
- *    8b      88    88 88~~~      88            88    88    88      88      88    88 8b      88~~~88 88           88~~~   88`8b   88    88     88  88~~~~~ 8b         88    
- *    Y8b  d8 `8b  d8' 88         88            88    `8b  d8'      88booo. `8b  d8' Y8b  d8 88   88 88booo.      88      88 `88. `8b  d8' db. 88  88.     Y8b  d8    88    
- *     `Y88P'  `Y88P'  88         YP            YP     `Y88P'       Y88888P  `Y88P'   `Y88P' YP   YP Y88888P      88      88   YD  `Y88P'  Y8888P  Y88888P  `Y88P'    YP    
- *                                                                                                                                                                          
- *                                                                                                                                                                          
- */
-
-
-/***
  *    d88888b db    db d8888b.  .d88b.  d8888b. d888888b      d8888b. d8888b.  .d88b.  d8888b. .d8888. 
  *    88'     `8b  d8' 88  `8D .8P  Y8. 88  `8D `~~88~~'      88  `8D 88  `8D .8P  Y8. 88  `8D 88'  YP 
  *    88ooooo  `8bd8'  88oodD' 88    88 88oobY'    88         88oodD' 88oobY' 88    88 88oodD' `8bo.   
