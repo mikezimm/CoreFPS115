d19d4f2a03b1140f1ee58b067a2bf94a34485849
mikezimm <49648086+mikezimm@users.noreply.github.com>
2022-08-10 21:13:59 -0400

    Update About.tsx

--- src/webparts/fpsCore115Banner/components/HelpPanel/About.tsx
+++ src/webparts/fpsCore115Banner/components/HelpPanel/About.tsx
@@ -1,8 +1,8 @@
 import * as React from 'react';
 
 import { IHelpTable, } from '../../fpsReferences';
-import { IRepoLinks, repoLink, } from '../../fpsReferences';
-import { convertIssuesMarkdownStringToSpan } from '../../fpsReferences';
+import { repoLink, } from '../../fpsReferences';
+// import { convertIssuesMarkdownStringToSpan } from '../../fpsReferences';
 
 import { createAboutRow } from '../../fpsReferences';
 
